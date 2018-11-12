0dd15e0f2f2930f2b69412f382f9baa59233958f
NeKz <nekzor@users.noreply.github.com>
2018-11-12 15:52:57 +0100

    Add formatter and auto format all files

---
+++ backend/formatter/formatter.js
@@ -0,0 +1,45 @@
+const beautify = require('js-beautify').html,
+    fs = require('fs');
+
+var path = '../../';
+
+var files = [
+    'stats.html',
+    'history.html',
+    'yearly.html',
+    'lp.html',
+    'seum.html',
+    'skill.html',
+    'glitches.html',
+    'mel.html',
+    'parser.html',
+    'cvars/hl2.html',
+    'cvars/p1.html',
+    'cvars/p2.html',
+    'cvars/sar.html',
+    'cvars/tbg.html',
+    'cvars/tsp.html'
+];
+
+var format = {
+    indent_size: 2,
+    space_in_empty_paren: true,
+    wrap_attributes: 'auto',
+    operator_position: 'after-newline',
+    'js': {
+        'indent_size': 4
+    },
+};
+
+for (let file of files) {
+    console.log(file);
+    fs.readFile(path + file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            fs.writeFile(path + file, beautify(data, format), (err) => {
+                if (err) console.error(err);
+            });
+        }
+    });
+}
