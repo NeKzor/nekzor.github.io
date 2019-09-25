79463171cfef251fbea65318558559da8d636501
NeKz <nekzor@users.noreply.github.com>
2019-09-25 08:46:54 +0200

    Move backend stuff to new branch

---
+++ iverb/iverb.cs
@@ -0,0 +1,66 @@
+﻿//#define EOYS
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Portal2Boards;
+using Portal2Boards.API;
+using Portal2Boards.Extensions;
+
+namespace nekzor.github.io
+{
+    internal static class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss")}] [iverb] {message}");
+        }
+        public static Task LogPortal2Boards(object _, LogMessage message)
+        {
+            Log(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+
+    internal static class iVerb
+    {
+        public static Portal2BoardsClient Client;
+        public static ChangelogQueryBuilder ChangelogBuilder;
+    }
+
+    internal static class App
+    {
+        public static readonly string Version = "nekzor.github.io/1.0";
+        public static readonly string Destination = $"{System.IO.Path.GetDirectoryName(typeof(App).Assembly.Location)}/../../";
+
+        private static async Task Main()
+        {
+            Logger.Log($"Version: {Version}");
+            Logger.Log($"Destination: {Destination}");
+
+            iVerb.Client = new Portal2BoardsClient(Version, true, 15);
+            iVerb.Client.Log += Logger.LogPortal2Boards;
+            iVerb.ChangelogBuilder = new ChangelogQueryBuilder();
+#if EOYS
+            iVerb.ChangelogBuilder.WithMaxDaysAgo(400);
+#else
+            iVerb.ChangelogBuilder.WithMaxDaysAgo(3333);
+#endif
+
+            var stats = new Stats();
+            await stats.Build();
+            await stats.ExportPage("stats.html");
+
+#if !EOYS
+            var history = new History();
+            await history.Build();
+            await history.ExportPage("history.html");
+
+            var yearly = new Yearly();
+            await yearly.Build();
+            await yearly.ExportPage("yearly.html");
+#endif
+        }
+    }
+}
---
+++ seum/Logger.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Portal2Boards;
+using SteamCommunity;
+
+namespace nekzor.github.io
+{
+    internal static class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss")}] [seum] {message}");
+        }
+        public static Task LogSteamCommunityClient(object _, SteamCommunity.LogMessage message)
+        {
+            Log(message.ToString());
+            return Task.CompletedTask;
+        }
+        public static Task LogPortal2BoardsClient(object _, Portal2Boards.LogMessage message)
+        {
+            Log(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
