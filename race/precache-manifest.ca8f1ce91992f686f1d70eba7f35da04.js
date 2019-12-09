538218df4fd5a59cf0d089197bc895d4b8093b2b
NeKz <nekzor@users.noreply.github.com>
2019-12-10 00:47:48 +0100

    Deploy experimental race chart project

---
+++ race/precache-manifest.ca8f1ce91992f686f1d70eba7f35da04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba3d47cdcad53251fb9b07ca90d0733",
+    "url": "/race/index.html"
+  },
+  {
+    "revision": "55fbeeef7434efea4945",
+    "url": "/race/static/js/2.a0a2f2a2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/race/static/js/2.a0a2f2a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db0cac7adfb6978168b3",
+    "url": "/race/static/js/main.8d0fa4bb.chunk.js"
+  },
+  {
+    "revision": "45a4943117f5dc067230",
+    "url": "/race/static/js/runtime-main.e5dd7684.js"
+  }
+]);
