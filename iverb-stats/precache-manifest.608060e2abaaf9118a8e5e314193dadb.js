12ce907c89137807b344196920049acb30b193d2
NeKz <nekzor@users.noreply.github.com>
2020-09-09 22:45:08 +0200

    wip stats

---
+++ iverb-stats/precache-manifest.608060e2abaaf9118a8e5e314193dadb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d547131c20ffe3c1821f7f42cd323ed",
+    "url": "/iverb-stats/index.html"
+  },
+  {
+    "revision": "b9a8a473561023203bed",
+    "url": "/iverb-stats/static/js/2.a9275322.chunk.js"
+  },
+  {
+    "revision": "f8689dbc61183e4eb9874953e6bca1c7",
+    "url": "/iverb-stats/static/js/2.a9275322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af38c081ab81201807d5",
+    "url": "/iverb-stats/static/js/main.1b16e43a.chunk.js"
+  },
+  {
+    "revision": "00ecca218162ed958e27",
+    "url": "/iverb-stats/static/js/runtime-main.7fed1f9c.js"
+  }
+]);
